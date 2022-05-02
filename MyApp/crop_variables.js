a8c0f4d4e091bbe5a875d1242152bf4d08de76c8
Ruchita Motwani <rmotwani69@gmail.com>
2022-05-03 00:02:54 +0530

    just another commit

--- MyApp/crop_variables.js
+++ MyApp/crop_variables.js
@@ -2,5 +2,5 @@ var crop_env_vars = {
    LOCATION: "us-central1",
    PROJECT: "suspashta-khetee",
    ENDPOINT_ID: "938411184074260480",
-   TOKEN: "ya29.A0ARrdaM9Vax7880ak5fqcGeyMmVH68JemvFJ5Pt_BuxIoSjFVnclzyFGeUGznW5mn5pODVgLYbXiRK2szkRytiPZTi4NqWVpORB9lv-_5Mo1a28WOTZbAhdY3dqpgPbKfTB0K5PSLkqqB_E7mt_AWHkkQb7BksA"
+   TOKEN: ""# enter your token generated
  };
